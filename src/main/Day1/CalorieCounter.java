028b906fbc76679f0eb6fe25fa9f07fbf51249f4
Driandis <maximiliangross@gmx.de>
2023-10-23 16:30:40 +0200

    Changed solution, start for over engin

--- src/main/Day1/CalorieCounter.java
+++ src/main/Day1/CalorieCounter.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import tools.MyFileReader;
 
@@ -7,11 +8,33 @@ public class CalorieCounter {
 
     //private MyFileReader reader = new MyFileReader();
     private static final String INPUT = "input/calories.txt";
+    private static final String TEST = "input/check.txt";
     public static void main(String[] args) {
 
-       MyFileReader reader = new MyFileReader();
-       reader.readFile(INPUT);
+        ArrayList<Integer> temp;
+        int current = 0;
+        int max = 0;
+        int hits = 0;
 
+       MyFileReader reader = new MyFileReader();
+       temp = reader.readFile(INPUT);
+       for(int i = 1; i < temp.size(); i++){
+           //System.out.println(temp.get(i));
+           if(temp.get(i) == null){
+               if(current > max) {
+                   max = current;
+                   hits++;
+                   current = 0;
+               }
+               else {
+                   current = 0;
+               }
+           }
+           else {
+               current = current + temp.get(i);
+           }
+       }
+       System.out.println("Maximum calories: " + max + " Hits: " + hits);
     }
 
 
--- src/main/Day1/tools/MyFileReader.java
+++ src/main/Day1/tools/MyFileReader.java
@@ -1,23 +1,29 @@
 package tools;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MyFileReader {
 
-    public void readFile(String filePath) {
+    public ArrayList<Integer> readFile(String filePath) {
         ClassLoader classLoader = MyFileReader.class.getClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream(filePath);
+        ArrayList<Integer> values = new ArrayList<>();
 
         if(inputStream != null) {
             try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
                 String line;
 
                 while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
+                    if(line.isEmpty()) {
+                        values.add(null);
+                    }
+                    else {
+                        values.add(Integer.parseInt(line));
+                    }
                 }
             }
             catch (IOException e) {
@@ -27,5 +33,6 @@ public class MyFileReader {
         else {
             System.err.println("File not found");
         }
+        return values;
     }
 }
