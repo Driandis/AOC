b69e1ad61669805cf60e887ee41b8b89131d0d9e
Driandis <maximiliangross@gmx.de>
2023-10-23 15:14:47 +0200

    read quiz input

--- src/main/Day1/CalorieCounter.java
+++ src/main/Day1/CalorieCounter.java
@@ -1,9 +1,18 @@
+import java.io.File;
+import java.io.InputStream;
+
 import tools.MyFileReader;
 
 public class CalorieCounter {
 
-    private MyFileReader reader = new MyFileReader();
+    //private MyFileReader reader = new MyFileReader();
+    private static final String INPUT = "input/calories.txt";
+    public static void main(String[] args) {
 
+       MyFileReader reader = new MyFileReader();
+       reader.readFile(INPUT);
+
+    }
 
 
 }
--- src/main/Day1/tools/MyFileReader.java
+++ src/main/Day1/tools/MyFileReader.java
@@ -3,29 +3,29 @@ package tools;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MyFileReader {
 
     public void readFile(String filePath) {
-        try {
-            // Create a FileReader to read the file
-            FileReader fileReader = new FileReader(filePath);
+        ClassLoader classLoader = MyFileReader.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(filePath);
 
-            // Wrap the FileReader in a BufferedReader for efficient reading
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+        if(inputStream != null) {
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                String line;
 
-            String line;
-
-            // Read and print each line in the file
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
             }
-
-            // Close the BufferedReader and FileReader when you're done
-            bufferedReader.close();
-            fileReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.err.println("File not found");
         }
     }
 }
