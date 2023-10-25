7dc551af11be417fc57e5213d2f29e36ec79ca83
Driandis <maximiliangross@gmx.de>
2023-10-25 15:30:56 +0200

    Added InputExtractor to fill backpacks with SonarChanges

---
+++ src/main/java/aoc/day1/InputExtractor.java
@@ -0,0 +1,42 @@
+package aoc.day1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import aoc.day1.model.Backpack;
+import tools.MyFileReader;
+public class InputExtractor {
+    private static final String INPUT = "aoc/day1/input/calories.txt";
+
+    private ArrayList<Integer> allFoodItems;
+    private ArrayList<Backpack> allBackpacks = new ArrayList<>();
+    public List<Integer> extractAll(){
+        MyFileReader reader = new MyFileReader();
+        allFoodItems = reader.readFile(INPUT);
+        return allFoodItems;
+    }
+
+    public List<Backpack> getBackpacks(List<Integer> list){
+        int number = 0;
+        int calories = 0;
+        if(list.isEmpty()) {
+            return new ArrayList<>();
+        }
+        allBackpacks.add(Backpack.createBackpack(number));
+        for (Integer integer : list) {
+            if (integer != null) {
+                allBackpacks.get(number)
+                        .addFood(integer);
+                calories += integer;
+            } else {
+                allBackpacks.get(number)
+                        .setCalories(calories);
+                //Just to test the toString() method
+                //System.out.println(allBackpacks.get(number).toString());
+                calories = 0;
+                allBackpacks.add(Backpack.createBackpack(++number));
+            }
+        }
+        return allBackpacks;
+    }
+}
